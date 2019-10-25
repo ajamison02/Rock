6dee04bfa842b6b9b37a0069ae95f44575250f17
ajamison02 <57016210+ajamison02@users.noreply.github.com>
2019-10-25 15:14:36 -0400

    Added Project
    Rock project

---
+++ Assets/Scripts/Rockcontroller.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rockcontroller : MonoBehaviour
+{
+
+
+    public string RockName = "Rochtholomeu";
+    public float hunger = 100f;
+    public float friendship = 100f;
+    public bool isSick = false;
+
+    float hungerScale = 2f;
+    float friendshipScale = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(RockName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StatusUpdate();
+        IsDead();
+        
+    }
+
+    void StatusUpdate()
+    {
+        hunger -= Time.deltaTime * hungerScale;
+        friendship -= Time.deltaTime * friendshipScale;
+
+        int sickChance = Random.Range(0, 100);
+        if (sickChance == 1)
+        {
+            isSick = true;
+            
+        }
+
+        if (isSick == false)
+        {
+            hungerScale = 2f;
+            friendshipScale = 2f;
+        }
+        else
+        {
+            hungerScale = 20f;
+            friendshipScale = 20f;
+        }
+    }
+
+    void IsDead()
+    {
+        if (hunger <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else if (friendship <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
